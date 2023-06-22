b00117847943496f9221322e69bd2d66091f1d97
Denis Finkevich <modern-nm@yandex.by>
2023-06-22 14:52:04 +0300

    initial

---
+++ Content.Server/ADT/ItemsToEmotionsSystem.cs
@@ -0,0 +1,18 @@
+/// <summary>
+/// This system can be used for send emote-message to clients when some custom item was used.
+///
+/// by ModerN, mail-to: h
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Server.ADT
+{
+    internal class ItemsToEmotionsSystem
+    {
+    }
+}
